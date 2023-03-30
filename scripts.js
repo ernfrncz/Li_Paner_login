63bd583b88f99f51b7ba2c1afe3deef0c2faf416
ernfrncz <126535224+ernfrncz@users.noreply.github.com>
2023-03-30 20:28:51 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function validateForm() {
+    var username = document.forms["loginForm"]["username"].value;
+    var password = document.forms["loginForm"]["password"].value;
+    if (username == "" && password == "") {
+        console.log("Login error.");
+        document.getElementById("log").innerHTML = "Please enter your username and password.";
+        return false;
+    } else if (username == "") {
+        console.log("Login error.");
+        document.getElementById("log").innerHTML = "Please enter your username.";
+        return false;
+    } else if (password == "") {
+        console.log("Login error.");
+        document.getElementById("log").innerHTML = "Please enter your password.";
+        return false;
+    }
+}
