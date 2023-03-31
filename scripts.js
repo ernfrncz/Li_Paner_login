47b71cfa916473cc85ac5ef83f463922a8320cdb
ernfrncz <126535224+ernfrncz@users.noreply.github.com>
2023-03-31 19:40:02 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,18 +4,13 @@ function validateForm() {
     if (username == "" && password == "") {
         console.log("Login error.");
         document.getElementById("log").innerHTML = "Please enter your username and password.";
-        return false;
     } else if (username == "") {
         console.log("Login error.");
         document.getElementById("log").innerHTML = "Please enter your username.";
-        return false;
     } else if (password == "") {
         console.log("Login error.");
         document.getElementById("log").innerHTML = "Please enter your password.";
-        return false;
-    } else {
-        window.location.replace("Li_Paner_2qProj/index.html");
-        history.back();
-        return true;
+    } else  if (username != "" && password != ""){
+        window.location.href = "2Q/index.html";
     }
 }
